21e883a4c997eb4b7406357fc0d7843cea44d604
javivi156 <javierarandacar2o>
2022-01-15 17:19:51 +0100

    Clases del 10 al 16 de Enero -Añadido toolbar reemplazando la actionbar -Añadida navegacion usando el NavigationDrawer -Eliminada la navegacion mediante dashboardfragment

--- app/src/main/java/com/example/inventory/MainActivity.java
+++ app/src/main/java/com/example/inventory/MainActivity.java
@@ -15,29 +15,89 @@ import android.view.View;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
 
-    private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setSupportActionBar(binding.content.toolbar);
         setContentView(binding.getRoot());
+        getSupportActionBar().setHomeAsUpIndicator(android.R.drawable.ic_menu_sort_by_size);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        setUpNavigationView();
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+    private void setUpNavigationView() {
+        binding.navigationView.setCheckedItem(R.id.action_dependencyList);
+        binding.navigationView.setNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.action_inventory:
+                    showAddInventory();
+                    break;
+                case R.id.action_dependencyList:
+                    showDependencyList();
+                    break;
+                case R.id.action_sections:
+                    showSections();
+                    break;
+                case R.id.action_products:
+                    showProducts();
+                    break;
+                case R.id.action_aboutus:
+                    showAboutUs();
+                    break;
+                case R.id.action_settings:
+                    showSettings();
+                    break;
+            }
+            binding.drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        });
+    }
+
+    private void showSettings() {
+        navController.navigate(R.id.settingsFragment);
+        binding.navigationView.getCheckedItem().setChecked(false);
+        binding.navigationView.setCheckedItem(R.id.action_settings);
+    }
+
+    private void showAboutUs() {
+        navController.navigate(R.id.aboutFragment);
+        binding.navigationView.getCheckedItem().setChecked(false);
+        binding.navigationView.setCheckedItem(R.id.action_aboutus);
+    }
+
+    private void showProducts() {
+        navController.navigate(R.id.dependencyListFragment);
+    }
+
+    private void showSections() {
+        navController.navigate(R.id.dependencyListFragment);
+    }
+
+    private void showDependencyList() {
+        navController.navigate(R.id.dependencyListFragment);
+    }
+
+    private void showAddInventory() {
+        binding.navigationView.getCheckedItem().setChecked(false);
+        binding.navigationView.setCheckedItem(R.id.action_inventory);
+        navController.navigate(R.id.addInventoryFragment);
     }
 
     @Override
@@ -50,18 +110,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
-            case R.id.action_about:
-                Toast.makeText(this, "Opcion menu about", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.action_settings:
-                Toast.makeText(this, "Opcion menu settings", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.action_search:
-                Toast.makeText(this, "Opcion menu search", Toast.LENGTH_SHORT).show();
+            case android.R.id.home:
+                binding.drawerLayout.openDrawer(GravityCompat.START);
                 return true;
             default:
                 //Return false indica que no se ha pulsado ninguna opcion del menu de la activity, pero que se habrá pulsado alguno modificado.
                 return false;
         }
+    }
+
+    @Override
+    public boolean onPreferenceStartFragment(PreferenceFragmentCompat caller, Preference pref) {
+        if (pref.getKey().equals("Account")){
+            navController.navigate(R.id.action_settingsFragment_to_accountFragment);
+        }
+        return true;
     }
 }
